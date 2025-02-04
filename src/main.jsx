b340992eae6ba1a41272097ae4d2e816fd1391f1
Sonia Binty <sonianazninbinty@gmail.com>
2025-02-04 08:51:28 +0600

    added navbar

---
+++ src/main.jsx
@@ -0,0 +1,26 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import Home from './pages/Home';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home></Home>,
+  },
+]);
+
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+   <RouterProvider router={router} />
+  </StrictMode>,
+)
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./componants/Navbar";
+
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar></Navbar>
+     
+    </div>
+  );
+};
+
+export default Home;
